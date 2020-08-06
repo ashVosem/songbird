c2a01d7cee3cc5859007d32774a8a2b8053f8464
Anton Yarashevich <ash.vosem@gmail.com>
2020-08-06 20:26:25 +0300

    feat: colorize dots v1

--- src/components/birds/Birds.jsx
+++ src/components/birds/Birds.jsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
 import birdsData from '../../assets/data/birdsData';
 
+import winSound from '../../assets/audio/win.mp3';
+import errorSound from '../../assets/audio/error.mp3';
+
 const Birds = ({
   RANDOM_SET_OF_BIRDS,
   RANDOM_BIRD,
   showBird,
   updateScore,
   isBirdPredicted,
-  isBirdClicked,
   showClickedBird,
 }) => {
-  // let points = birdsData[RANDOM_SET_OF_BIRDS].length;
   const [points, setPoints] = useState(birdsData[RANDOM_SET_OF_BIRDS].length);
-  const [clickedBird, setClickedBird] = useState(null);
+  const [dotColors, setDotColors] = useState({
+    0: 'default',
+    1: 'default',
+    2: 'default',
+    3: 'default',
+    4: 'default',
+    5: 'default',
+  });
 
   return (
     <div className="birds">
@@ -20,21 +28,30 @@ const Birds = ({
         {birdsData[RANDOM_SET_OF_BIRDS].map((bird, index) => (
           <li
             onClick={() => {
+              const win = new Audio(winSound);
+              const error = new Audio(errorSound);
+
               showClickedBird(index);
-              setClickedBird(index);
+
               if (!isBirdPredicted) {
                 setPoints(points > 0 ? points - 1 : 0);
                 if (RANDOM_BIRD === index) {
                   showBird();
                   updateScore(points);
+                  setDotColors({ ...dotColors, [index]: 'predicted' });
+                  win.play();
+                } else {
+                  setDotColors({ ...dotColors, [index]: 'not_predicted' });
+                  error.play();
                 }
               }
+              console.log(dotColors);
             }}
             className={'birds--list-' + index}
             key={'bird' + index}
           >
             <p>
-              <span></span>
+              <span className={dotColors[index]}></span>
               {RANDOM_BIRD === bird.id - 1 ? bird.name + '+' : bird.name}
             </p>
           </li>
